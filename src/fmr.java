17b7c8ef2fc16e4d36af8fc2de0bdd349c44bb09
nitai4andra <kolobaev.dv@gmail.com>
2017-10-13 09:57:22 +0300

    Add files via upload

--- src/fmr.java
+++ src/fmr.java
@@ -1,4 +1,4 @@
-import java.io.BufferedWriter;
+﻿import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,6 +18,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 
 public class fmr {
@@ -46,26 +48,37 @@ public class fmr {
 				eElement = (Element) nNode;
 				/*// Use this to test in a real browser (though web-sites are treating HtmlUnitDriver differently)
 				driver = new FirefoxDriver();*/
-				driver = new HtmlUnitDriver();
-				driver.get("http://files.mail.ru/");
+				//driver = new HtmlUnitDriver(BrowserVersion.BEST_SUPPORTED);
+				//((HtmlUnitDriver) driver).setJavascriptEnabled(true);
+				System.setProperty("webdriver.gecko.driver","/home/nitai4andra/soft/geckodriver-v0.19.0-linux64/geckodriver");
+				driver = new FirefoxDriver();
+							
+				//driver.get("https://e.mail.ru/login");
+				driver.get("https://mail.ru/");
+				//Thread.sleep(100);
 				
-				login = driver.findElement(By.name("Login"));
-				pass = driver.findElement(By.name("Password"));
+				login = driver.findElement(By.name("login"));
+				pass = driver.findElement(By.name("password"));
 				
 				login.sendKeys(getTagValue("login", eElement));
 				pass.sendKeys(getTagValue("password", eElement));
 				driver.findElement(By.xpath("//input[@value='Войти']")).click();				
 				/* After authorization in HtmlUnitDriver we would be redirected to our mail page
 				(mail.ru), so we need to get back to files.mail.ru again */
-				driver.get("http://files.mail.ru/");
-								
+				driver.get("http://files.mail.ru/ls/1");
+				//Thread.sleep(100);				
 				/*//Use this to understand what html page site is sending you looks like
 				 
 				FileWriter fstream = new FileWriter("log.html");
 				BufferedWriter out = new BufferedWriter(fstream);
 				out.write(driver.getPageSource());
 				out.close();*/
-								
+				
+				FileWriter fstream = new FileWriter("log.html");
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(driver.getPageSource());
+				out.close();
+				
 				driver.findElement(By.xpath("//a[@href='/ls/4']")).click();
 				filesNumber = filesCount();
 				if (filesNumber > 0) {
